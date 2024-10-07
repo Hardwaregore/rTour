6d312625cea94c276a7eb7e03bea9f5d8736cbc0
Hardwaregore <hardwaregore101@gmail.com>
2024-10-06 19:03:14 -0500

    Added motor control and SD reader support

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,38 @@
 #include <Arduino.h>
 #include <ArduinoSTL.h>
+#include <SPI.h>
+#include <SD.h>
+
+File configFile;
+
+int m1p1 = 2;
+int m1p2 = 3;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(7, OUTPUT);
+  pinMode(m1p1, OUTPUT);
+  pinMode(m1p2, OUTPUT);
+  pinMode(8, OUTPUT);
+  /*
+  Serial.begin(9600);
+  if (!SD.begin(53)) {
+    Serial.println("There was an error");
+    while (1);
+  }
+  Serial.println("Success");
+
+  configFile = SD.open("config.txt");
+  if (configFile) {
+    while (configFile.available()){
+      Serial.println(configFile.readString());
+    }
+  } else {
+    Serial.println("There was an error");
+  }
+  */
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  digitalWrite(7, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(7, LOW);
-  delay(500);
+  digitalWrite(m1p1, HIGH);
+  digitalWrite(m1p2, LOW);
+  analogWrite(8, 255);
 }
